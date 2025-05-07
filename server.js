2965baad41cc3f552dd29e530ed59121678736fe
enlilj <156503770+enlilj@users.noreply.github.com>
2025-05-07 18:51:24 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+const users = {};
+
+io.on('connection', (socket) => {
+  console.log('🔌 مستخدم متصل:', socket.id);
+
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    users[socket.id] = roomId;
+    socket.to(roomId).emit('user-connected', socket.id);
+  });
+
+  socket.on('offer', ({ to, offer }) => {
+    io.to(to).emit('offer', { from: socket.id, offer });
+  });
+
+  socket.on('answer', ({ to, answer }) => {
+    io.to(to).emit('answer', { from: socket.id, answer });
+  });
+
+  socket.on('ice-candidate', ({ to, candidate }) => {
+    io.to(to).emit('ice-candidate', { from: socket.id, candidate });
+  });
+
+  socket.on('disconnect', () => {
+    const roomId = users[socket.id];
+    socket.to(roomId).emit('user-disconnected', socket.id);
+    delete users[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`🚀 السيرفر يعمل على المنفذ ${PORT}`);
+});
