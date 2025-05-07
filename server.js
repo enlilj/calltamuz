3d0daf5cce32b780c8673d43dac134a0c1cb1f7e
enlilj <156503770+enlilj@users.noreply.github.com>
2025-05-07 21:18:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -17,35 +17,32 @@ const io = new Server(server, {
 const users = {};
 
 io.on('connection', socket => {
-  console.log('🔌 متصل جديد:', socket.id);
-
   socket.on('join-room', roomId => {
     socket.join(roomId);
     users[socket.id] = roomId;
     socket.to(roomId).emit('user-connected', socket.id);
+  });
 
-    socket.on('offer', ({ to, offer }) => {
-      io.to(to).emit('offer', { from: socket.id, offer });
-    });
+  socket.on('offer', ({ to, offer }) => {
+    io.to(to).emit('offer', { from: socket.id, offer });
+  });
 
-    socket.on('answer', ({ to, answer }) => {
-      io.to(to).emit('answer', { from: socket.id, answer });
-    });
+  socket.on('answer', ({ to, answer }) => {
+    io.to(to).emit('answer', { from: socket.id, answer });
+  });
 
-    socket.on('ice-candidate', ({ to, candidate }) => {
-      io.to(to).emit('ice-candidate', { from: socket.id, candidate });
-    });
+  socket.on('ice-candidate', ({ to, candidate }) => {
+    io.to(to).emit('ice-candidate', { from: socket.id, candidate });
+  });
 
-    socket.on('disconnect', () => {
-      const room = users[socket.id];
-      socket.to(room).emit('user-disconnected', socket.id);
-      delete users[socket.id];
-      console.log('❌ قطع الاتصال:', socket.id);
-    });
+  socket.on('disconnect', () => {
+    const roomId = users[socket.id];
+    socket.to(roomId).emit('user-disconnected', socket.id);
+    delete users[socket.id];
   });
 });
 
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
-  console.log(`🚀 السيرفر يعمل على http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
